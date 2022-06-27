fc629a802d5630e4fec3c410ebfc019b3f298ea4
KDH5706 <rpaehdgns12@naver.com>
2022-06-27 17:05:28 +0900

    220627 오후 commit

--- Socket/hello_server.c
+++ Socket/hello_server.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 	struct sockaddr_in clnt_addr;
 	socklen_t clnt_addr_size;
 
-	char message[] = "Hellow World!";
+	char message[] = "Hello World!";
 
 	if(argc != 2)
 	{
